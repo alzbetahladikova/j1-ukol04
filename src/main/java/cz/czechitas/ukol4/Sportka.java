b4cec4b74dae7e344fcf8b2bc5325fc59218f6b9
alzbetahladikova <beta.strelbova@gmail.com>
2023-03-14 21:09:11 +0100

    naplněn list

--- src/main/java/cz/czechitas/ukol4/Sportka.java
+++ src/main/java/cz/czechitas/ukol4/Sportka.java
@@ -3,45 +3,54 @@ package cz.czechitas.ukol4;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
+
+import static java.util.Collections.shuffle;
 
 /**
  * Třída pro losování Sportky.
  */
 public class Sportka {
-  private final List<Integer> osudi = new ArrayList<>();
+    private final List<Integer> osudi = new ArrayList<>();
 
-  public Sportka() {
-    //TODO naplnit osudí čísly 1 až 49.
-  }
+    public Sportka() {
+        for (int i = 0; i < 49; i++) {
+            osudi.add(i + 1);
+        }
+    }
 
-  /**
-   * Zamíchá osudí.
-   *
-   * @see Collections#shuffle(List)
-   */
-  public void zamichat() {
-    //TODO zamíchat osudí
-  }
+    /**
+     * Zamíchá osudí.
+     *
+     * @see Collections#shuffle(List)
+     */
+    public void zamichat() {
+        //TODO zamíchat osudí
 
-  /**
-   * Vrátí seznam s 6 vylosovanými čísly.
-   *
-   * @return Seznam vylosovaných čísel.
-   *
-   * @see List#subList(int, int)
-   */
-  public List<Integer> vylosovanaCisla() {
-    //TODO Vrátit seznam prvních 6 čísel z osudí.
-    return null;
-  }
 
-  /**
-   * Vrátí sedmé vylosované číslo (dodatkové číslo)
-   *
-   * @return Dodatkové číslo.
-   */
-  public Integer dodatkoveCislo() {
-    //TODO Vrátit sedmé číslo z osudí.
-    return null;
-  }
+
+    }
+
+    /**
+     * Vrátí seznam s 6 vylosovanými čísly.
+     *
+     * @return Seznam vylosovaných čísel.
+     * @see List#subList(int, int)
+     */
+    public List<Integer> vylosovanaCisla() {
+        //TODO Vrátit seznam prvních 6 čísel z osudí.
+        return null;
+    }
+
+    /**
+     * Vrátí sedmé vylosované číslo (dodatkové číslo)
+     *
+     * @return Dodatkové číslo.
+     */
+    public Integer dodatkoveCislo() {
+        //TODO Vrátit sedmé číslo z osudí.
+        return null;
+    }
+
+
 }
